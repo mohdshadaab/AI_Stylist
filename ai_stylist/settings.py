e8f5682303fe1bb8d875f7711ad2f6672331968a
Mohammad Shadaab <mohdshadaab27@gmail.com>
2022-04-21 19:17:17 +0530

    Update settings.py

--- ai_stylist/settings.py
+++ ai_stylist/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b3k-r62spoc--lxc6f-bpl7!(oy3sc-r4v@=i3+7jgj=34bm(c'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
